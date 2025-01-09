c17d1a594d05494f42cce49248eaedd1673dc8e5
a.samoshuk <a.samoshuk@rit.va>
2025-01-09 12:12:27 +0300

    save

---
+++ MyYndexMusic/Cashe/CasheClass.cs
@@ -0,0 +1,10 @@
+using MyYndexMusic.Model;
+using Yandex.Music.Api.Models.Playlist;
+
+namespace MyYndexMusic.Cashe;
+
+public static class CasheClass
+{
+    public static List<PlayListModel> _playlists;
+    public static List<YPlaylist> _yPlaylists;
+}
